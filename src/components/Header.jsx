278f3d6f611dbf309cad36b2c9e78b102da7f232
arsistemadpw13 <arsistemadpw@gmail.com>
2025-12-03 13:49:47 -0400

    Segundo commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({ darkMode, toggleTheme }) => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#">Supabase</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
